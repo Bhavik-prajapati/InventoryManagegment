9820b55305db478c596af9ed2c95aa910955ce3e
RavalRahul <ravalrahul586@gmail.com>
2024-06-20 21:47:23 +0530

    folder files modified

--- index.php
+++ index.php
@@ -1,82 +1,175 @@
+<?php
+  include("connection.php");
+session_start();
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Country State City Dropdown</title>
-    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-</head>
-<body>
-    <h1>Select Country, State, and City</h1>
-    <form>
-        <label for="country">Country:</label>
-        <select id="country" name="country">
-            <option value="">Select Country</option>
-            <!-- Populate countries from the database -->
-            <?php
-            $conn = new mysqli('localhost', 'root', '', 'myapp');
-            if ($conn->connect_error) {
-                die("Connection failed: " . $conn->connect_error);
-            }
-            
-            $sql = "SELECT id, name FROM countries";
-            $result = $conn->query($sql);
-            while ($row = $result->fetch_assoc()) {
-                echo "<option value='{$row['id']}'>{$row['name']}</option>";
-            }
-            
-            $conn->close();
-            ?>
-        </select>
-        <br><br>
-        
-        <label for="state">State:</label>
-        <select id="state" name="state">
-            <option value="">Select State</option>
-        </select>
-        
-        <br><br>
-        
-        <label for="city">City:</label>
-        <select id="city" name="city">
-            <option value="">Select City</option>
-        </select>
-    </form>
 
-    <script>
-        $(document).ready(function() {
-            $('#country').change(function() {
-                var country_id = $(this).val();
-                $.ajax({
-                    url: 'get_states.php',
-                    type: 'POST',
-                    data: { country_id: country_id },
-                    success: function(data) {
-                        $('#state').html('<option value="">Select State</option>');
-                        var states = JSON.parse(data);
-                        states.forEach(function(state) {
-                            $('#state').append('<option value="' + state.id + '">' + state.name + '</option>');
-                        });
-                    }
-                });
-            });
-            
-            $('#state').change(function() {
-                var state_id = $(this).val();
-                $.ajax({
-                    url: 'get_cities.php',
-                    type: 'POST',
-                    data: { state_id: state_id },
-                    success: function(data) {
-                        $('#city').html('<option value="">Select City</option>');
-                        var cities = JSON.parse(data);
-                        cities.forEach(function(city) {
-                            $('#city').append('<option value="' + city.id + '">' + city.name + '</option>');
-                        });
-                    }
-                });
-            });
-        });
-    </script>
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+  <title>Pages / Login - NiceAdmin Bootstrap Template</title>
+  <meta content="" name="description">
+  <meta content="" name="keywords">
+
+  <!-- Favicons -->
+  <link href="assets/img/favicon.png" rel="icon">
+  <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+
+  <!-- Google Fonts -->
+  <link href="https://fonts.gstatic.com" rel="preconnect">
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Nunito:300,300i,400,400i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+
+  <!-- Vendor CSS Files -->
+  <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+  <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+  <link href="assets/vendor/quill/quill.snow.css" rel="stylesheet">
+  <link href="assets/vendor/quill/quill.bubble.css" rel="stylesheet">
+  <link href="assets/vendor/remixicon/remixicon.css" rel="stylesheet">
+  <link href="assets/vendor/simple-datatables/style.css" rel="stylesheet">
+
+  <!-- Template Main CSS File -->
+  <link href="assets/css/style.css" rel="stylesheet">
+
+  <!-- =======================================================
+  * Template Name: NiceAdmin
+  * Template URL: https://bootstrapmade.com/nice-admin-bootstrap-admin-html-template/
+  * Updated: Mar 17 2024 with Bootstrap v5.3.3
+  * Author: BootstrapMade.com
+  * License: https://bootstrapmade.com/license/
+  ======================================================== -->
+</head>
+
+<body>
+
+  <main>
+    <div class="container">
+
+      <section class="section register min-vh-100 d-flex flex-column align-items-center justify-content-center py-4">
+        <div class="container">
+          <div class="row justify-content-center">
+            <div class="col-lg-4 col-md-6 d-flex flex-column align-items-center justify-content-center">
+
+              <div class="d-flex justify-content-center py-4">
+                <a href="index.html" class="logo d-flex align-items-center w-auto">
+                  <img src="assets/img/logo.png" alt="">
+                  <span class="d-none d-lg-block">NiceAdmin</span>
+                </a>
+              </div><!-- End Logo -->
+
+              <div class="card mb-3">
+
+                <div class="card-body">
+
+                  <div class="pt-4 pb-2">
+                    <h5 class="card-title text-center pb-0 fs-4">Login to Your Account</h5>
+                    <p class="text-center small">Enter your username & password to login</p>
+                  </div>
+
+                  <form  method="POST" action=""  class="row g-3 needs-validation" novalidate>
+
+                    <div class="col-12">
+                      <label for="yourUsername" class="form-label">Username</label>
+                      <div class="input-group has-validation">
+                        <span class="input-group-text" id="inputGroupPrepend">@</span>
+                        <input type="text" name="username" class="form-control" id="yourUsername" required>
+                        <div class="invalid-feedback">Please enter your username.</div>
+                      </div>
+                    </div>
+
+                    <div class="col-12">
+                      <label for="yourPassword" class="form-label">Password</label>
+                      <input type="password" name="password" class="form-control" id="yourPassword" required>
+                      <div class="invalid-feedback">Please enter your password!</div>
+                    </div>
+
+                    <div class="col-12">
+                      <div class="form-check">
+                        <input class="form-check-input" type="checkbox" name="remember" value="true" id="rememberMe">
+                        <label class="form-check-label" for="rememberMe">Remember me</label>
+                      </div>
+                    </div>
+                    <div class="col-12">
+                      <button name="login-btn" class="btn btn-primary w-100" type="submit">Login</button>
+                    </div>
+                    <div class="col-12">
+                      <p class="small mb-0">Don't have account? <a href="pages-register.html">Create an account</a></p>
+                    </div>
+                  </form>
+
+                </div>
+              </div>
+
+              <div class="credits">
+                <!-- All the links in the footer should remain intact. -->
+                <!-- You can delete the links only if you purchased the pro version. -->
+                <!-- Licensing information: https://bootstrapmade.com/license/ -->
+                <!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/nice-admin-bootstrap-admin-html-template/ -->
+                Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a>
+              </div>
+
+            </div>
+          </div>
+        </div>
+
+      </section>
+
+    </div>
+  </main><!-- End #main -->
+
+  <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+
+  <!-- Vendor JS Files -->
+  <script src="assets/vendor/apexcharts/apexcharts.min.js"></script>
+  <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="assets/vendor/chart.js/chart.umd.js"></script>
+  <script src="assets/vendor/echarts/echarts.min.js"></script>
+  <script src="assets/vendor/quill/quill.min.js"></script>
+  <script src="assets/vendor/simple-datatables/simple-datatables.js"></script>
+  <script src="assets/vendor/tinymce/tinymce.min.js"></script>
+  <script src="assets/vendor/php-email-form/validate.js"></script>
+
+  <!-- Template Main JS File -->
+  <script src="assets/js/main.js"></script>
+
 </body>
+
 </html>
+
+
+<?php
+
+
+if (isset($_POST['login-btn'])) {
+  $username = $_POST['username'];
+  $password = $_POST['password'];
+  $remember = isset($_POST['remember']);
+
+  $username = mysqli_real_escape_string($conn, $username);
+  $password = mysqli_real_escape_string($conn, $password);
+
+  $query = "SELECT * FROM admin_master WHERE username='$username' AND password='$password'";
+  $result = mysqli_query($conn, $query);
+  $row = mysqli_fetch_array($result, MYSQLI_ASSOC);
+  
+  $count = mysqli_num_rows($result);
+
+  if ($count == 1) {
+    $_SESSION['username'] = $username;
+    if ($remember) {
+      setcookie("username", $username, time() + (86400 * 30), "/"); // 86400 = 1 day
+      setcookie("password", $password, time() + (86400 * 30), "/"); // For demonstration purposes only
+  }
+    
+    echo "<script>window.location = 'dashboard.php';</script>";
+  } else {
+    echo "<script>alert('Login failed. Incorrect username or password.');</script>";
+    echo "<script>window.location = 'admin-login.php';</script>";
+  }
+}
+
+?>

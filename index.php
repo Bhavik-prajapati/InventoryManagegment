e332226b32799a76b18a805a1b2428b3953d619e
RavalRahul <ravalrahul586@gmail.com>
2024-06-21 21:10:52 +0530

    new changes

--- index.php
+++ index.php
@@ -51,7 +51,7 @@ session_start();
               <div class="d-flex justify-content-center py-4">
                 <a href="index.html" class="logo d-flex align-items-center w-auto">
                   <img src="assets/img/logo.png" alt="">
-                  <span class="d-none d-lg-block">NiceAdmin</span>
+                  <span class="d-none d-lg-block">User Login</span>
                 </a>
               </div><!-- End Logo -->
 
@@ -91,7 +91,7 @@ session_start();
                       <button name="login-btn" class="btn btn-primary w-100" type="submit">Login</button>
                     </div>
                     <div class="col-12">
-                      <p class="small mb-0">Don't have account? <a href="pages-register.html">Create an account</a></p>
+                      <!-- <p class="small mb-0">Don't have account? <a href="pages-register.html">Create an account</a></p> -->
                     </div>
                   </form>
 
@@ -99,11 +99,7 @@ session_start();
               </div>
 
               <div class="credits">
-                <!-- All the links in the footer should remain intact. -->
-                <!-- You can delete the links only if you purchased the pro version. -->
-                <!-- Licensing information: https://bootstrapmade.com/license/ -->
-                <!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/nice-admin-bootstrap-admin-html-template/ -->
-                Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a>
+                <a href="admin">Admin login</a>
               </div>
 
             </div>
